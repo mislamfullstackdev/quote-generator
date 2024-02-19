fd48de23fc5f12f63c8b3affbf10bfc49f201fc9
mislamfullstackdev <mislam.pasona@gmail.com>
2024-02-19 13:25:39 +0900

    added style for the APP
    The content is still static

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1> React Project - Qoute generator using Free API </h1>
+      <section>
+        <button className="button"> New Qoute </button>
+        <h3>
+          <span>“</span>
+            {//quoute will go here
+            }
+          <span>“</span>
+        </h3>
+        <i> Toylor Example{
+          //Author will goes here
+        }</i>
+      </section>
+    </main>
   );
 }
 
